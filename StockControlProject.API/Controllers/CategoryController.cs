fbebe9d840a435c7edcad4365af0dbcfb0d2ed50
ghost <ghost@GHOST2023>
2024-03-28 15:53:13 +0300

    mertcana sevgiler

--- StockControlProject.API/Controllers/CategoryController.cs
+++ StockControlProject.API/Controllers/CategoryController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using StockControlProject.Entities.Entities;
 using StockControlProject.Service.Abstract;
 
@@ -15,10 +16,51 @@ namespace StockControlProject.API.Controllers
         {
             _service = service;
         }
+
         [HttpGet]
         public IActionResult TumKategorileriGetir()
         {
             return Ok(_service.GetAll());
+        }
+
+        [HttpGet]
+        public IActionResult AktifKategorileriGetir()
+        {
+            return Ok(_service.GetActive());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult IdyeGoreKategorileriGetir(int id)
+        {
+            return Ok(_service.GetById(id));
+        }
+
+        [HttpPost]
+        public IActionResult KategoriEkle(Category category)
+        {
+            _service.Add(category);
+            return CreatedAtAction("IdyeGoreKategorileriGetir", new { id = category.Id }, category);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult KategoriGuncelle(int id, Category category)
+        {
+            if (id != category.Id) return BadRequest();
+            try
+            {
+                if (_service.Update(category)) return Ok();
+                return NotFound();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoryExist(id)) return NotFound();
+            }
+            return NoContent();
+        }
+
+        private bool CategoryExist(int id)
+        {
+            return _service.Any(x => x.Id == id);
         }
     }
 }
